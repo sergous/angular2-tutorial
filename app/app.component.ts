216de329e13ada3f09a3f4e474b5aaa5fbd77286
Sergey Smirnov <s@tigr.bz>
2016-06-07 19:45:34 +0300

    5.2 add dashboard.

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,8 +2,15 @@ import {Component} from '@angular/core';
 import {HeroService} from "./hero.service";
 import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router-deprecated';
 import {HeroesComponent} from "./heroes.component";
+import {DashboardComponent} from "./dashboard.component";
 
 @RouteConfig([
+    {
+        path: '/dashboard',
+        name: 'Dashboard',
+        component: DashboardComponent,
+        useAsDefault: true
+    },
     {
         path: '/heroes',
         name: 'Heroes',
@@ -15,7 +22,10 @@ import {HeroesComponent} from "./heroes.component";
     selector: 'my-app',
     template: `
         <h1>{{title}}</h1>
-        <a [routerLink]="['Heroes']">Heroes</a>
+        <nav>
+            <a [routerLink]="['Dashboard']">Dashboard</a>
+            <a [routerLink]="['Heroes']">Heroes</a>
+        </nav>
         <router-outlet></router-outlet>
     `,
     directives: [ROUTER_DIRECTIVES],
