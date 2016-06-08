829f5fabb465f997dbe64e0f8e9a8989307aa510
Sergey Smirnov <s@tigr.bz>
2016-06-08 17:38:44 +0300

    6 http request to in-memory-backend.

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,17 +1,30 @@
 import { Injectable } from '@angular/core';
-import {HEROES} from "./mock-heroes";
 import {Hero} from "./hero";
+import 'rxjs/add/operator/toPromise';
+import { Http, Response } from '@angular/http';
 
 @Injectable()
 export class HeroService {
+
+    constructor (private http: Http) {}
+
+    private heroesUrl = 'app/heroes'; // URL to the web api
+
+    private handleError(error: any) {
+        console.error('An error occurred', error);
+        return Promise.reject(error.message || error);
+    }
+
+    getHeroes(): Promise<Hero[]> {
+        return this.http.get(this.heroesUrl)
+            .toPromise()
+            .then(res =>
+                res.json().data)
+            .catch(this.handleError);
+    }
+
     getHero(id: number) {
         return this.getHeroes()
             .then(heroes => heroes.filter(hero => hero.id === id)[0]);
-    }
-    getHeroes() {
-        return Promise.resolve(HEROES);
-    }
-    getHeroesSlowly() {
-        return new Promise<Hero[]>(resolve => setTimeout(()=>resolve(HEROES), 2000)); //2 seconds delay
     }
 }
--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,22 @@
+// In memory API
+import {provide} from '@angular/core';
+import {XHRBackend} from '@angular/http';
+import {InMemoryBackendService, SEED_DATA} from 'angular2-in-memory-web-api'
+import {InMemoryDataService} from './in-memory-data.service';
+
+// App bootstrap
 import {bootstrap}    from '@angular/platform-browser-dynamic';
+import {HTTP_PROVIDERS} from '@angular/http';
 import {AppComponent} from './app.component';
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+    HTTP_PROVIDERS,
+    // in-mem server
+    provide(XHRBackend, {
+        useClass: InMemoryBackendService
+    }),
+    // in-mem server data
+    provide(SEED_DATA, {
+        useClass: InMemoryDataService
+    })
+]);
