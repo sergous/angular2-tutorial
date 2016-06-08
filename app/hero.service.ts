74ec518ac71234a97f3a70f271c5d6e91668c9cb
Sergey Smirnov <s@tigr.bz>
2016-06-08 18:21:37 +0300

    6.1 heroes crud.

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -15,6 +15,43 @@ export class HeroService {
         return Promise.reject(error.message || error);
     }
 
+    // Update existing Hero
+    private put(hero: Hero) {
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+
+        let url = '${this.heroesUrl}/${hero.id}';
+
+        return this.http
+            .put(url, JSON.stringify(hero), {headers: headers})
+            .toPromise()
+            .then(() => hero)
+            .catch(this.handleError);
+    }
+
+    // Delete existing Hero
+    delete(hero: Hero) {
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+
+        let url = '${this.heroesUrl}/${hero.id}';
+
+        return this.http
+            .delete(url, headers)
+            .toPromise()
+            .catch(this.handleError);
+    }
+
+    save() {
+        this.heroService
+            .save(this.hero)
+            .then(hero => {
+                this.hero = hero; // saved hero, w/ id if new
+                this.goBack(hero);
+            })
+            .catch(error => this.error = error); // TODO: Display error message
+    }
+
     getHeroes(): Promise<Hero[]> {
         return this.http.get(this.heroesUrl)
             .toPromise()
@@ -26,5 +63,10 @@ export class HeroService {
     getHero(id: number) {
         return this.getHeroes()
             .then(heroes => heroes.filter(hero => hero.id === id)[0]);
+    }
+
+    goBack(savedHero: Hero = null) {
+        this.close.emit(savedHero);
+        if (this.navigated) { window.history.back(); }
     }
 }
